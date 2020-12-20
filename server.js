c6cb4ca5619ef35eba83e317cda9508675079beb
Amine <aminemouhaouir1996@gmail.com>
2020-12-20 20:33:04 +0100

    last4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app=express();
 
-app.use(express.static(__dirname+'/src'));
+app.use(express.static(__dirname+'/src/app'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname+'/src/index.html'));
