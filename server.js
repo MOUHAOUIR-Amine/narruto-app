9496f6904aad4b1ce4300a071f703ef732254f43
Amine <aminemouhaouir1996@gmail.com>
2020-12-20 19:35:21 +0100

    last3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app=express();
 
-app.use(express.static(__dirname+'/dist/narruto-app/src'));
+app.use(express.static(__dirname+'/dist/narruto-app/src/app'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname+'/dist/narruto-app/src/index.html'));
