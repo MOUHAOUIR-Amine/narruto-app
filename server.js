ca8b6ffc7fa6042f386aa1bf35995f655b35c379
Amine <aminemouhaouir1996@gmail.com>
2020-12-20 17:42:25 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app=express();
+
+app.use(express.static(__dirname+'/dist/narruto-app'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/narruto-app/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);git 
