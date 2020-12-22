d7e377d502a78442c2cf028b0c11686db0e16b65
Amine <aminemouhaouir1996@gmail.com>
2020-12-22 10:38:13 +0100

    gg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {AngularFireModule} from '@angular/fire'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCVMHrJ8ZgjrPa8FmBsQs39TcYq9irZG1A",
+      authDomain: "cours-tp-mouhaouir.firebaseapp.com",
+      projectId: "cours-tp-mouhaouir",
+      storageBucket: "cours-tp-mouhaouir.appspot.com",
+      messagingSenderId: "529117110089",
+      appId: "1:529117110089:web:474aa78f2742a723240498"
+    }))
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
